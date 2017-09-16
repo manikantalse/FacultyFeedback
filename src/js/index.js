f85faa30231b3cd6a8f2a464bed1ead84337d876
manikanta <manikanta.lse@gmail.com>
2017-09-16 07:45:23 +0530

    changed index js

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,63 @@
 var id_no = document.getElementById("IDNO");
 var pass = document.getElementById("pass");
 var sub = document.getElementById('sub');
-var error=true;
-function setValues(roll_no) {
-    if (roll_no.toLowerCase().indexOf("165u1a") != -1)
-        document.getElementById('year').value = 2;
-    if (roll_no.toLowerCase().substring(7, 8) == '5')
-        document.getElementById('branch').value = 'CSE';
-    if (roll_no.toLowerCase().substring(7, 8) == '4')
-        document.getElementById('branch').value = 'ECE';
-    if (roll_no.toLowerCase().substring(7, 8) == '3')
-        document.getElementById('branch').value = 'MECH';
-    if (roll_no.toLowerCase().substring(7, 8) == '2')
-        document.getElementById('branch').value = 'CIVIL';
-    if (typeof roll_no.toLowerCase().substring(8, 9) == 'string') {
-        document.getElementById('section').value = 'B';
-    }
+var login = document.getElementById('login');
 
-    else if (parseInt(roll_no.toLowerCase().substring(8, 10)) > 60)
-        document.getElementById('section').value = 'B';
-    if (parseInt(roll_no.toLowerCase().substring(8, 10)) < 61)
-        document.getElementById('section').value = 'A';
-}
+
 $(document).ready(function () {
-    
+    $('#sub').on('click', function (e) {
+        e.preventDefault();
+        $.get("isUser.php?username=" + id_no.value + "&password=" + pass.value, function (data) {
+            data = data.trim();
+            if (data == 'true') {
 
-    $('#sub').click(function (e) {
-        if(error){
-            e.preventDefault();
-        }
-        
-        console.log("isUser.php?username="+id_no.value+"&password="+pass.value);
-            $.get("isUser.php?username="+id_no.value+"&password="+pass.value,function(data){
-                    data = data.trim();
-                    if(data=='true'){
-                        error = false;
-                        sub.click();
-                    }else{
-                        if ($('#IDNO').val() == '') {
-                            $('#rollError').show();
-                        } else {
-                            $('#rollError').hide();
-                            $('#passError').show();
-                        }            
-                    }
-            })
-            
-            
+                var roll_no = document.getElementById('IDNO').value;
+                var str = roll_no.toLowerCase();
+                switch (0) {
+                    case str.indexOf('175u1a0'): document.getElementById('year').value = 1; break;
+                    case str.indexOf('165u1a0'): document.getElementById('year').value = 2; break;
+                    case str.indexOf('155u1a0'): document.getElementById('year').value = 3; break;
+                    case str.indexOf('145u1a0'): document.getElementById('year').value = 4; break;
+                }
+                if (roll_no.toLowerCase().substring(7, 8) == '5') {
+                    document.getElementById('branch').value = 'CSE';
+
+                    if (parseInt(roll_no.toLowerCase().substring(8, 10)) < 61)
+                        document.getElementById('section').value = 'A';
+                    else
+                        document.getElementById('section').value = 'B';
+
+                }
+                if (roll_no.toLowerCase().substring(7, 8) == '4') {
+                    document.getElementById('branch').value = 'ECE';
+                    if (parseInt(roll_no.toLowerCase().substring(8, 10)) < 51)
+                        document.getElementById('section').value = 'A';
+                    else
+                        document.getElementById('section').value = 'B';
+                }
+                if (roll_no.toLowerCase().substring(7, 8) == '3') {
+                    document.getElementById('branch').value = 'MECH';
+                    document.getElementById('section').value = 'A';
+                }
+                if (roll_no.toLowerCase().substring(7, 8) == '1') {
+                    document.getElementById('branch').value = 'CIVIL';
+                    document.getElementById('section').value = 'A';
+                }
+
+                login.submit();
+            } else {
+                if ($('#IDNO').val() == '') {
+                    $('#rollError').show();
+                } else {
+                    $('#rollError').hide();
+                    $('#passError').show();
+                }
+            }
+        })
 
     });
-});
+
+})
+
+
+
